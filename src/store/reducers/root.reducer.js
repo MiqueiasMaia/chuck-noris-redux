6bf7a26b8686a0c206a72ee0820e7a62a9a61b4a
Miqueias Maia <manoel.maia@robbyson.com>
2024-05-21 16:30:08 -0300

    feat: add jokeReducer to root reducer

--- src/store/reducers/root.reducer.js
+++ src/store/reducers/root.reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counterReducer from './counter.reducer';
+import jokeReducer from './joke.reducer';
 
 const rootReducer = combineReducers({
   counterReducer,
+  jokeReducer
 });
 
 export default rootReducer;
